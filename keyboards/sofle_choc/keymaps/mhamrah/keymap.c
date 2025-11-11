0f672257c16a0c62e7bd0ff465d85a752aeffa3c
Mike Hamrah <m@hamrah.com>
2025-11-11 10:15:19 -0500

    mhamrah Sofle Choc layout with oled animations

--- keyboards/sofle_choc/keymaps/mhamrah/keymap.c
+++ keyboards/sofle_choc/keymaps/mhamrah/keymap.c
@@ -26,6 +26,22 @@
 
 char rgb_str[10];
 
+// Custom keycodes and aliases
+enum custom_keycodes {
+    RGB_NEXT = SAFE_RANGE,
+};
+
+// Short aliases for RGB Matrix keycodes (for visual alignment in keymap)
+#define RGB_PREV QK_RGB_MATRIX_MODE_PREVIOUS
+#define RGB_HUI QK_RGB_MATRIX_HUE_UP
+#define RGB_HUD QK_RGB_MATRIX_HUE_DOWN
+#define RGB_SAI QK_RGB_MATRIX_SATURATION_UP
+#define RGB_SAD QK_RGB_MATRIX_SATURATION_DOWN
+#define RGB_VAI QK_RGB_MATRIX_VALUE_UP
+#define RGB_VAD QK_RGB_MATRIX_VALUE_DOWN
+#define RGB_SPI QK_RGB_MATRIX_SPEED_UP
+#define RGB_SPD QK_RGB_MATRIX_SPEED_DOWN
+
 // Get RGB mode name using QMK's RGB_MATRIX_* constants so indices remain correct
 const char *get_rgb_mode_name(uint8_t mode) {
     switch (mode) {
@@ -109,61 +125,34 @@ void oled_write_bitmap_24x24(const uint8_t *bitmap, uint8_t start_col, uint8_t s
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌────┬────┬────┬────┬────┬────┐                         ┌────┬────┬────┬────┬────┬────┐
-     * │ `  │ 1  │ 2  │ 3  │ 4  │ 5  │                         │ 6  │ 7  │ 8  │ 9  │ 0  │ -  │
-     * ├────┼────┼────┼────┼────┼────┤                         ├────┼────┼────┼────┼────┼────┤
-     * │Tab │ Q  │ W  │ E  │ R  │ T  │                         │ Y  │ U  │ I  │ O  │ P  │ =  │
-     * ├────┼────┼────┼────┼────┼────┤                         ├────┼────┼────┼────┼────┼────┤
-     * │Ctrl│ A  │ S  │ D  │ F  │ G  │                         │ H  │ J  │ K  │ L  │ ;  │ '  │
-     * ├────┼────┼────┼────┼────┼────┼────┐               ┌────┼────┼────┼────┼────┼────┼────┤
-     * │Shft│ B  │ Z  │ X  │ C  │ V  │Mute│               │Play│ N  │ M  │ ,  │ .  │ /  │Shft│
-     * └────┴────┴────┴────┴────┴────┴────┘               └────┴────┴────┴────┴────┴────┴────┘
-     *                ┌────┬────┬────┬────┐               ┌────┬────┬────┬────┐
-     *                │RGB │LSA │Opt │Sym │               │Util│Ctrl│Cmd │    │
-     *                │Mod │Tab │Esc │ L1 │               │ L2 │Tab │Spc │    │
-     *                └────┴────┼────┼────┤               ├────┼────┼────┴────┘
-     *                          │Cmd │Spc │               │Ent │    │
-     *                          │Spc │    │               │    │    │
-     *                          └────┴────┘               └────┴────┘
-     */
-    [_MAC_DEFAULT] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_EQUAL, KC_RCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_B, KC_Z, KC_X, KC_C, KC_V, KC_MUTE, KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, RGB_MOD, LSA_T(KC_TAB), LOPT_T(KC_ESC), MO(1), LCMD_T(KC_SPC), KC_SPC, KC_ENT, LCTL_T(KC_TAB), MO(2), SCMD(KC_SPC)),
+    [_MAC_DEFAULT] = LAYOUT(
+    KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                                          KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINUS,
+    KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                          KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_EQUAL,
+    KC_RCTL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                          KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_B,   KC_Z,   KC_X,   KC_C,   KC_V, KC_MUTE,                        KC_MPLY, KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 
-    /*
-     * ┌────┬────┬────┬────┬────┬────┐                         ┌────┬────┬────┬────┬────┬────┐
-     * │    │    │    │    │    │    │                         │    │    │    │    │    │Esc │
-     * ├────┼────┼────┼────┼────┼────┤                         ├────┼────┼────┼────┼────┼────┤
-     * │    │    │    │    │    │    │                         │    │ (  │ )  │ [  │ ]  │    │
-     * ├────┼────┼────┼────┼────┼────┤                         ├────┼────┼────┼────┼────┼────┤
-     * │    │    │PgDn│PgUp│    │    │                         │    │Left│Down│ Up │Rght│ \  │
-     * ├────┼────┼────┼────┼────┼────┼────┐               ┌────┼────┼────┼────┼────┼────┼────┤
-     * │    │    │    │    │    │    │    │               │    │    │    │    │    │    │    │
-     * └────┴────┴────┴────┴────┴────┴────┘               └────┴────┴────┴────┴────┴────┴────┘
-     *                ┌────┬────┬────┬────┐               ┌────┬────┬────┬────┐
-     * │    │    │    │▓▓▓▓│               │    │    │    │    │
-     *                └────┴────┼────┼────┤               ├────┼────┼────┴────┘
-     *                          │    │BkSp│               │    │    │
-     *                          └────┴────┘               └────┴────┘
-     */
-    [_MAC_SYMBOLS] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ESC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_PAGE_DOWN, KC_PAGE_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_BSLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+        RGB_NEXT, LSA_T(KC_TAB), LOPT_T(KC_ESC), MO(1), LCMD_T(KC_SPC),        KC_SPC, KC_ENT, LCTL_T(KC_TAB), MO(2), SCMD(KC_SPC)
+    ),
 
-    /*
-     * ┌────┬────┬────┬────┬────┬────┐                         ┌────┬────┬────┬────┬────┬────┐
-     * │RGB │ F1 │ F2 │ F3 │ F4 │ F5 │                         │ F6 │ F7 │ F8 │ F9 │ F10│Boot│
-     * ├────┼────┼────┼────┼────┼────┤                         ├────┼────┼────┼────┼────┼────┤
-     * │RGB │Hue+│Sat+│Brt+│Spd+│ F11│                         │ F12│    │    │    │    │EClr│
-     * ├────┼────┼────┼────┼────┼────┤                         ├────┼────┼────┼────┼────┼────┤
-     * │RGB │Hue-│Sat-│Brt-│Spd-│    │                         │    │    │    │    │    │    │
-     * ├────┼────┼────┼────┼────┼────┼────┐               ┌────┼────┼────┼────┼────┼────┼────┤
-     * │    │    │    │    │    │    │RGB │               │Play│    │    │    │    │    │    │
-     * └────┴────┴────┴────┴────┴────┴────┘               └────┴────┴────┴────┴────┴────┴────┘
-     *                ┌────┬────┬────┬────┐               ┌────┬────┬────┬────┐
-     *                │    │    │    │    │               │▓▓▓▓│    │    │    │
-     *                └────┴────┼────┼────┤               ├────┼────┼────┴────┘
-     *                          │    │    │               │    │    │
-     *                          └────┴────┘               └────┴────┘
-     */
-    [_MAC_UTIL] = LAYOUT(RGB_TOG, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, QK_BOOT, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EE_CLR, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+
+    [_MAC_SYMBOLS] = LAYOUT(
+    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS, KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ESC,
+    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS, KC_TRNS,                               KC_TRNS, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_PAGE_DOWN, KC_PAGE_UP, KC_TRNS, KC_TRNS,                               KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, KC_BSLS,
+    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+
+                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                           KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+
+
+    [_MAC_UTIL] = LAYOUT(
+        RGB_NEXT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                 KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  QK_BOOT,
+        RGB_NEXT, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_F11,                                KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EE_CLR,
+        RGB_PREV, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_NEXT, KC_MPLY,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+
+                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    )
 
 };
 // clang-format on
@@ -172,19 +161,81 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PAGE_DOWN, KC_PAGE_UP)},
     {ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-    {ENCODER_CCW_CW(RGB_RMOD, RGB_MOD), ENCODER_CCW_CW(RGB_HUD, RGB_HUI)},
+    {ENCODER_CCW_CW(RGB_PREV, RGB_NEXT), ENCODER_CCW_CW(RGB_HUD, RGB_HUI)},
 };
 #endif
 
+// Helper to identify RGB Matrix control keycodes (new QMK API)
+static bool is_rgb_matrix_keycode(uint16_t keycode) {
+    switch (keycode) {
+        case QK_RGB_MATRIX_ON:
+        case QK_RGB_MATRIX_OFF:
+        case QK_RGB_MATRIX_TOGGLE:
+        case QK_RGB_MATRIX_MODE_NEXT:
+        case QK_RGB_MATRIX_MODE_PREVIOUS:
+        case QK_RGB_MATRIX_HUE_UP:
+        case QK_RGB_MATRIX_HUE_DOWN:
+        case QK_RGB_MATRIX_SATURATION_UP:
+        case QK_RGB_MATRIX_SATURATION_DOWN:
+        case QK_RGB_MATRIX_VALUE_UP:
+        case QK_RGB_MATRIX_VALUE_DOWN:
+        case QK_RGB_MATRIX_SPEED_UP:
+        case QK_RGB_MATRIX_SPEED_DOWN:
+            return true;
+        default:
+            return false;
+    }
+}
 // Global key counter for OLED display
 static uint16_t key_count = 0;
 
 // Handle custom keycodes
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // Handle RGB_NEXT custom keycode
+    if (keycode == RGB_NEXT && record->event.pressed) {
+        if (!rgb_matrix_is_enabled()) {
+            // If RGB is off, turn it on and set to first mode
+            rgb_matrix_enable();
+            rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+        } else {
+            uint8_t current_mode = rgb_matrix_get_mode();
+            // Cycle through specific modes, then turn off
+            switch (current_mode) {
+                case RGB_MATRIX_SOLID_COLOR:
+                    rgb_matrix_mode(RGB_MATRIX_CYCLE_UP_DOWN);
+                    break;
+                case RGB_MATRIX_CYCLE_UP_DOWN:
+                    rgb_matrix_mode(RGB_MATRIX_CYCLE_OUT_IN);
+                    break;
+                case RGB_MATRIX_CYCLE_OUT_IN:
+                    rgb_matrix_mode(RGB_MATRIX_CYCLE_PINWHEEL);
+                    break;
+                case RGB_MATRIX_CYCLE_PINWHEEL:
+                    rgb_matrix_mode(RGB_MATRIX_DUAL_BEACON);
+                    break;
+                case RGB_MATRIX_DUAL_BEACON:
+                    rgb_matrix_mode(RGB_MATRIX_RAINBOW_PINWHEELS);
+                    break;
+                case RGB_MATRIX_RAINBOW_PINWHEELS:
+                    rgb_matrix_mode(RGB_MATRIX_HUE_BREATHING);
+                    break;
+                case RGB_MATRIX_HUE_BREATHING:
+                    // Turn off RGB after last mode
+                    rgb_matrix_disable();
+                    break;
+                default:
+                    // If on an unlisted mode, go to first mode
+                    rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+                    break;
+            }
+        }
+        return false; // Skip further processing
+    }
+
     // Increment key counter for all keys except RGB control keys
     if (record->event.pressed) {
         // Don't count RGB control keys for key counter
-        if (!(keycode >= RGB_TOG && keycode <= RGB_MODE_TWINKLE)) {
+        if (!is_rgb_matrix_keycode(keycode) && keycode != RGB_NEXT) {
             key_count++;
         }
     }
