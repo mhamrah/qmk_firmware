ec7f4c5d3cff0fe322488f02127b66f67bbd4638
Mike Hamrah <m@hamrah.com>
2026-09-01 23:39:39 -0400

    Toggle Sofle RGB from right encoder

--- keyboards/sofle_choc/keymaps/mhamrah/keymap.c
+++ keyboards/sofle_choc/keymaps/mhamrah/keymap.c
@@ -32,7 +32,7 @@ enum custom_keycodes {
 
 // Short aliases for RGB Matrix keycodes (for visual alignment in keymap)
 #define RGB_PREV QK_RGB_MATRIX_MODE_PREVIOUS
-#define RGB_ON QK_RGB_MATRIX_ON
+#define RGB_TOG QK_RGB_MATRIX_TOGGLE
 #define RGB_HUI QK_RGB_MATRIX_HUE_UP
 #define RGB_HUD QK_RGB_MATRIX_HUE_DOWN
 #define RGB_SAI QK_RGB_MATRIX_SATURATION_UP
@@ -132,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                                          KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINUS,
     KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                          KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_EQUAL,
     KC_RCTL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                          KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_B,   KC_Z,   KC_X,   KC_C,   KC_V, KC_MUTE,                        RGB_ON,  KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LSFT, KC_B,   KC_Z,   KC_X,   KC_C,   KC_V, KC_MUTE,                        RGB_TOG, KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 
         RGB_NEXT, LSA_T(KC_TAB), LOPT_T(KC_ESC), MO(1), LCMD_T(KC_SPC),        KC_SPC, KC_ENT, LCTL_T(KC_TAB), MO(2), SCMD(KC_SPC)
     ),
@@ -152,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_NEXT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                 KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  QK_BOOT,
         RGB_NEXT, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_F11,                                KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EE_CLR,
         RGB_PREV, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_NEXT, RGB_ON,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_NEXT, RGB_TOG,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
